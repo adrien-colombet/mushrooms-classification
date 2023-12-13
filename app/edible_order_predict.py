dd6e56e4a2de6fabc713e7877addc7aa9ae07ec2
Adrien COLOMBET <adrien.colombet@gmail.com>
2023-12-13 12:50:00 +0100

    streamlit app --> model download and prediction

--- app/edible_order_predict.py
+++ app/edible_order_predict.py
@@ -3,15 +3,38 @@ from PIL import Image
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow import keras
-from tensorflow.keras.applications.mobilenet import preprocess_input
+from tensorflow.keras.applications.mobilenet import preprocess_input as mobile_preprocess_input
+from tensorflow.keras.applications.resnet_v2 import preprocess_input as resnet_preprocess_input
+from google_drive_downloader import GoogleDriveDownloader as gdd
+import os
+
+# downloading models
+url_order_model = '1KNN8xDcEVd0fMSp0JfgNaBUqOlumxaQY'
+dest_path = './model/MobileNetModel.hdf5'
+
+if not os.path.exists(dest_path):
+    gdd.download_file_from_google_drive(file_id=url_order_model,
+                                    dest_path=dest_path,
+                                    unzip=True)
+
+url_edibility_model = '1svf6jw1OdNTPKeh9zVyJiNJ9ElnMFI2j' 
+dest_path = './model/TL_ResNet50V2.hdf5'
+
+if not os.path.exists(dest_path):
+    gdd.download_file_from_google_drive(file_id=url_edibility_model,
+                                    dest_path=dest_path,
+                                    unzip=True)
 
 # Load the model
-model = load_model('./model_mobile_checkpoint')
+order_model = load_model('./model/MobileNetModel.hdf5')
+
+edbility_model = load_model('./model/TL_ResNet50V2.hdf5')
 
 # Create a file uploader to upload your images
 uploaded_file = st.file_uploader("Choose a jpg image...", type="jpg")
 
 # Classes
+edible_class_names = {0: "Non comestible", 1:"Comestible"}
 class_names = {0: "Agaricales", 1:"Auriculariales", 2:"Boletales", 3:"Cantharellales", 4:"Gomphales", 5:"Pezizales", 6:"Polyporales", 7:"Russulales"}
 
 if uploaded_file is not None:
@@ -30,7 +53,16 @@ if uploaded_file is not None:
     image = np.array(img)
     image = np.expand_dims(image, axis=0)
     
+    # Prediction on edibility
+    edbility_predictions = edbility_model.predict(resnet_preprocess_input(image))
+    st.write('Edibility:', edible_class_names[np.argmax(edbility_predictions)])
+    st.write(edbility_predictions)
+
     # Make predictions
-    predictions = model.predict(preprocess_input(image))
-    st.write('Predicted class:', class_names[np.argmax(predictions)])
+    predictions = order_model.predict(mobile_preprocess_input(image))
+    st.write('Predicted order:', class_names[np.argmax(predictions)])
     st.write(predictions)
+
+    
+
+
